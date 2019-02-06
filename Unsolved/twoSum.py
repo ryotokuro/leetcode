964e91a4174267d65436cc65368e4990fa3714e5
ryotokuro <ryotokuro@gmail.com>
2019-02-06 13:47:43 +1100

    added first leetcode problem: twoSum

---
+++ Unsolved/twoSum.py
@@ -0,0 +1,11 @@
+# PROBLEM
+# Given:
+# - an array of integers
+
+# Return:
+# - indices of the two numbers such that they add up to a SPECIFIC target.
+
+# Assume:
+# - each input would have exactly one solution, and you may not use the same element twice.
+
+# EXAMPLE
