7aefa520d1abe51f46124fabcaae3dc5c6420870
ryotokuro <ryotokuro@gmail.com>
2019-02-07 23:49:12 +1100

    added given function, will test soon

--- Unsolved/twoSum.py
+++ Unsolved/twoSum.py
@@ -14,3 +14,18 @@
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1].
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(nums):
+            for j in range(nums)
+
+            if == target:
+                low = i
+                high = j
+
+        return list(low, high)
