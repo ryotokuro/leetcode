d68c4480e2915b15cd018a9205ef7c8dc7d8babf
ryotokuro <ryotokuro@gmail.com>
2019-03-04 09:07:38 +1100

    added small portion of my plan of attack

--- Unsolved/addTwoNumbers.py
+++ Unsolved/addTwoNumbers.py
@@ -4,5 +4,12 @@
 #         self.val = x
 #         self.next = None
 
+# EXAMPLE
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# Explanation: 342 + 465 = 807.
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        string = None  # Have an empty string initially
+        # Get linked list value and INSERT it (add to front) to a string
