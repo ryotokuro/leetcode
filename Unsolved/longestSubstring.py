2a796673b1251ec718a80a2acce7f1602c610ee2
ryotokuro <ryotokuro@gmail.com>
2019-03-04 23:57:39 +1100

    added rough plan, will expand on it more

---
+++ Unsolved/longestSubstring.py
@@ -0,0 +1,8 @@
+# Input: "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+# PLAN
+# Consider substrings CASE by CASE
+# If repeated character, start new current count, store max count
+# At the end, compare maxCount with the current count
