4ea8d1bdbd2988bf00364018afa94d034cd1364e
ryotokuro <ryotokuro@gmail.com>
2019-03-04 23:43:21 +1100

    trying to figure out how to setup a linkedlist for testing

--- Unsolved/addTwoNumbers.py
+++ Unsolved/addTwoNumbers.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 # EXAMPLE
 # Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
@@ -13,3 +13,17 @@ class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         string = None  # Have an empty string initially
         # Get linked list value and INSERT it (add to front) to a string
+        print(l1.val)
+
+
+aList = ListNode(2)
+aList = aList.next
+aList.val = 4
+aList = aList.next
+aList.val = 3
+
+b = [5, 6, 4]
+
+answer = Solution()
+print(Solution.addTwoNumbers(answer, aList, b))
+
