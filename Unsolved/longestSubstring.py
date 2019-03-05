8d6a5d6f31aafb6d80e2c36cc7d3e4b28c04c144
ryotokuro <ryotokuro@gmail.com>
2019-03-05 11:05:45 +1100

    added input implementation and intialised variables which I'll need

--- Unsolved/longestSubstring.py
+++ Unsolved/longestSubstring.py
@@ -6,3 +6,25 @@
 # Consider substrings CASE by CASE
 # If repeated character, start new current count, store max count
 # At the end, compare maxCount with the current count
+
+# WHAT I NEED
+# currLen - tracks count of the case
+# maxLen - length of biggest substring
+# longest - stores the longest substring
+
+# OPTIONAL
+# substring - tracks current substring (not necessary but only for print purpose)
+# index - keep track of index :)
+def longestSubstring(string):
+    currLen, maxLen = 0, 0
+    longest, substring = "", ""
+
+
+
+    return maxLen
+
+
+# SETUP INPUT METHOD
+string = str(input("Input: "))
+print("Output:", longestSubstring(string))
+print("Explanation: The answer is " + longest, "with the length of", maxLen)
