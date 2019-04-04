49169371a32932512219ebd27d73bf10f06d7cc9
ryotokuro <ryotokuro@gmail.com>
2019-04-04 23:30:01 +1100

    might try a sliding window approach - but looks like the runtimeis dependent on length of the palindrome

--- Unsolved/longestPalindomicSubstring.py
+++ Unsolved/longestPalindomicSubstring.py
@@ -5,13 +5,69 @@
 # Output: "bab"
 # Note: "aba" is also a valid answer.
 
+# OWN EXAMPLE - sliding window
+sfdobabado
+j-1
+
+sfdobabad
+or
+fdobabado
+
+j-1
+
+sfdobaba
+fdobabad
+dobabado
+
+j-1
+
+sfdobab
+fdobaba
+dobabad
+obabado
+
+j-1
+
+sfdoba
+fdobab
+dobaba
+obabad
+babado
+
+j-1
+
+sfdob
+fdoba
+dobab
+obaba
+babad
+abado
+
+j-1
+
+sfdo
+fdob
+doba
+obab
+baba
+abad
+bado
+
+j-1
+
+sfd
+fdo
+dob
+oba
+bab STOP
 
 def longestPalindromicSubString(string):
     # make a copy of string reversed
     stringIndexes = dict()
 
-    i = 0
+    for i in range(len(string)):
     j = len(string)
+
     print(string[i:j])
     print(string[i:j][::-1])  # [::-1] is the reverse slicing
     # compare reversedString with string for longest substring
