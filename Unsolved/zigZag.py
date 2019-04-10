d83b85804916cf153a448cabc755d13f0191fb44
ryotokuro <ryotokuro@gmail.com>
2019-04-11 09:55:15 +1000

    set up initial things for function

---
+++ Unsolved/zigZag.py
@@ -0,0 +1,29 @@
+# PROBLEM
+# - Input: s = "PAYPALISHIRING", numRows = 3
+# - Output: "PAHNAPLSIIGYIR"
+#
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+
+# And then read line by line: "PAHNAPLSIIGYIR"
+#
+# Write the code that will take a string and make this conversion given a number of rows.
+
+
+def convert(s, numRows):  # returns the converted string
+    converted = ""
+
+    return converted
+
+
+print(convert("PAYPALISHIRING", 3))  # Test Case 1
+
+print("PAYPALISHIRING", 4)  # Test Case 2
+
+# User Input Methods
+string = str(input())
+numRows = int(input())
+
+print(convert(string, numRows))
