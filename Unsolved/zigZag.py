73c6eda8301b6dbcefe2d96f3ab25e2e714a40fa
ryotokuro <ryotokuro@gmail.com>
2019-04-11 12:02:50 +1000

    trying to determine a pattern I can use to consistently retrieve the pieces

--- Unsolved/zigZag.py
+++ Unsolved/zigZag.py
@@ -9,12 +9,35 @@
 
 # And then read line by line: "PAHNAPLSIIGYIR"
 #
-# Write the code that will take a string and make this conversion given a number of rows.
+# EXAMPLE 2
+# - Input: s = "PAYPALISHIRING", numRows = 4
+# - Output: "PINALSIGYAHRPI"
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
+#
+# Note: I notice that the diagonal is the same length as the number of rows
+# -> If I simply add on the number of rows - 2 (the fence posts) then I can get the first row simply
+# -> This also consistently retrieves another term for the subsequent rows, but I need to get the extra bits
+
+PAYPALISHIRING numrows = 5
+##
+# P       H
+# A     S I
+# Y   I   R
+# P L     I G
+# A       N
 
 
 def convert(s, numRows):  # returns the converted string
-    converted = ""
-
+    converted = "s[0]"
+    diagonal = numRows+1  # k should be the diagonal gap
+    # inter should be the bridge gap
+    for i in range(0, len(s)):  # go row by row
+        # try to append each
+        while i+k < len(s):  # ensure we don't go out of bounds
+            converted.append(s[i+k])  # PAHN - need to somehow make sure it goes to the second row
     return converted
 
 
