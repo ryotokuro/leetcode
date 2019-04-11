ee2c5969602edb96c71b7d6dbf9cd953bbf8638e
ryotokuro <ryotokuro@gmail.com>
2019-04-11 15:36:12 +1000

    modified method

--- Unsolved/zigZag.py
+++ Unsolved/zigZag.py
@@ -55,37 +55,45 @@ import math
 
 
 def convert(s, numRows):  # returns the converted string
-    converted = "s[0]"  # first entry is always the first letter
-    addA = addFL = len(s)+(math.floor(len(s)/2))
+    converted = s[0]  # first entry is always the first letter
+    addA = addFL = numRows+(math.floor(numRows/2))
     addB = 0
 
     for i in range(numRows):  # go row by row
+        j = i
+
         if i == 0 or i == numRows-1:  # first or last row
-            while i+addFL < len(s):
-                converted += s[i+addFL]
+            while j+addFL < len(s):
+                converted += s[j+addFL]
+                j += addFL
+                print(j, converted)
 
         else:  # its an intermediate row
             aTurn = True  # starts off with a being first
-            while i < len(s):
-                if aTurn and i+addA < len(s):  # alternate to B
-                    converted += s[i + addA]
-                    aTurn = not aTurn
+            print("A", addA)
+            if aTurn and i+addA < len(s):  # alternate to B
+                j += addA
+                converted += s[j]
+                aTurn = not aTurn
 
-                elif not aTurn and i+addB < len(s):
-                    converted += s[i + addB]
-                    aTurn = not aTurn
+            elif not aTurn and j+addB < len(s):
+                j += addB
+                converted += s[j]
+                aTurn = not aTurn
 
-                else:
-                    # next row
-                    addA -= 2  # need to decrease by 2
-                    addB += 2  # increase by 2
-                    break
+            else:
+                # next row
+                addA -= 2  # need to decrease by 2
+                addB += 2  # increase by 2
+                break
     return converted
 
 
 print(convert("PAYPALISHIRING", 3))  # Test Case 1
+# PAHNAPLSIIGYIR
 
 print(convert("PAYPALISHIRING", 4))  # Test Case 2
+# PINALSIGYAHRPI
 
 # User Input Methods
 # string = str(input())
