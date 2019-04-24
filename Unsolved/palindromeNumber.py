416c341f668de6c7990f599bd206d4207a92fa38
ryotokuro <ryotokuro@gmail.com>
2019-04-24 17:26:46 +1000

    perhaps use some fancy mathematics to extract the digits? but then for bigger numbers it would take longer O(len(n))

--- Unsolved/palindromeNumber.py
+++ Unsolved/palindromeNumber.py
@@ -1,13 +1,18 @@
 def isPalindrome(num: int):
+    # METHOD 1 (using string conversion)
+    # if str(num) == str(num)[::-1]:
+       # return True
+    # return False  # returns boolean true/false
 
-    return false  # returns boolean true/false
+    # METHOD 2 (no string)
+    return (num - (num % 10)) % 100
 
 
 # TEST CASES
-isPalindrome(121)   # true
-isPalindrome(-121)  # false
-isPalindrome(10)    # false
+print(isPalindrome(121))   # true
+print(isPalindrome(-121))  # false
+print(isPalindrome(10))    # false
 
 # Could you solve it without converting the integer to a string?
 num = int(input())
-isPalindrome(num)
+print(isPalindrome(num))
