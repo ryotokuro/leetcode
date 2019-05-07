5d2e182737097167ded00bffc88bad388475f0c6
ryotokuro <ryotokuro@gmail.com>
2019-05-08 00:00:01 +1000

    adding test

--- Unsolved/romanToInteger.py
+++ Unsolved/romanToInteger.py
@@ -30,3 +30,11 @@
 # Given a roman numeral, convert it to an integer.
 # Input is guaranteed to be within the range from 1 to 3999.
 
+
+def romanToInteger():
+    return 1
+
+
+romanToInteger("III")
+
+roman = str(input())
