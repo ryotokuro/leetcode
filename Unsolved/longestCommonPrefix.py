2813768fab8a6d634b2f0eb6f6f4ebeb6f72453a
ryotokuro <ryotokuro@gmail.com>
2019-05-10 09:53:02 +1000

    added tests, defining plan for function

--- Unsolved/longestCommonPrefix.py
+++ Unsolved/longestCommonPrefix.py
@@ -1,4 +1,12 @@
 # PROBLEM
 # Write a function to find the longest common prefix string amongst an array of strings.
 # Edge Case: If there is no common prefix, return an empty string "".
+# Note: All inputs are in lowercase letters a-z
 
+def longestCommonPrefix():
+# to avoid overflow, want to use the length of the smallest word in the list
+
+
+# TESTS
+print(longestCommonPrefix(["flower","flow","flight"]))  # Output "fl"
+print(longestCommonPrefix(["dog","racecar","car"]))     # Output: ""
