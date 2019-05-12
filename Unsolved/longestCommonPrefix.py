b0898e427662478bcfc1120f2f5368f22c547791
ryotokuro <ryotokuro@gmail.com>
2019-05-13 00:37:42 +1000

    added a flag to help break out of the nested loop and also dragged a condition out for assigning new string values

--- Unsolved/longestCommonPrefix.py
+++ Unsolved/longestCommonPrefix.py
@@ -7,16 +7,22 @@ def longestCommonPrefix(strings):
     # to avoid overflow, want to use the length of the smallest word in the list
     shortLen = min(strings, key=len)
     commonPrefix = ""
+    finished = False
 
     # procedurally need to compare letters
     for i in range(1, len(shortLen)):
         testPrefix = strings[0][:i]
 
         for j in range(1, len(strings)):
-            if strings[j][:i] != testPrefix:
-                break
-            else:
-                commonPrefix = strings[j][:1]
+            if strings[j][:i] != testPrefix:  # if any one of the strings dont match
+                finished = True  # trigger mismatch flag
+                break  # exit loop early to save time
+
+        if finished:
+            break
+
+        else:
+            commonPrefix = strings[0][:i]
 
     return commonPrefix  # return longest common prefix using first element in the list
 
