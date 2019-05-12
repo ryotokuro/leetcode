4de08eb2d0572ed415f64f49fc01b36729f2bdcb
ryotokuro <ryotokuro@gmail.com>
2019-05-12 23:58:15 +1000

    thinking now i might just use a sliding window actually instead of comparing letters

--- Unsolved/longestCommonPrefix.py
+++ Unsolved/longestCommonPrefix.py
@@ -6,11 +6,14 @@
 def longestCommonPrefix(strings):
     # to avoid overflow, want to use the length of the smallest word in the list
     shortLen = min(strings, key=len)
+    testPrefix = commonPrefix = ""
 
     # procedurally need to compare letters
     for i in range(shortLen):
+        testPrefix = strings[j][i]
         for j in range(len(strings)):
-            if strings[j][]
+
+            if strings[j][i] != testPrefix
 
     return strings[0[:shortLen]]  # return longest common prefix using first element in the list
 
