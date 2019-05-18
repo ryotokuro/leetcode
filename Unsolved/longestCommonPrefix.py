1147129e34cf746a1c39061093b7a8553aaebfeb
ryotokuro <ryotokuro@gmail.com>
2019-05-18 22:56:35 +1000

    added more legibility so code is clearer with comments

--- Unsolved/longestCommonPrefix.py
+++ Unsolved/longestCommonPrefix.py
@@ -5,32 +5,35 @@
 
 
 def longestCommonPrefix(strings):
-        # to avoid overflow, want to use the length of the smallest word in the list
-        commonPrefix = ""
-
+        # if there is only one string in the array
         if len(strings) == 1:
             return strings[0]
 
-        elif strings != []:
-            shortLen = min(strings, key=len)
-            finished = False
+        # else, there is more than one string in the array
+        else:
+            # to avoid overflow, we use the length of the smallest word in the list
+            commonPrefix = ""
 
-            # procedurally need to compare letters
-            for i in range(1, len(shortLen)):
-                testPrefix = strings[0][:i]
+            if strings != []:
+                shortLen = min(strings, key=len)
+                finished = False
 
-                for j in range(1, len(strings)):
-                    if strings[j][:i] != testPrefix:  # if any one of the strings dont match
-                        finished = True  # trigger mismatch flag
-                        break  # exit loop early to save time
+                # procedurally need to compare letters
+                for i in range(1, len(shortLen)):
+                    testPrefix = strings[0][:i]
 
-                if finished:
-                    break
+                    for j in range(1, len(strings)):
+                        if strings[j][:i] != testPrefix:  # if any one of the strings dont match
+                            finished = True  # trigger mismatch flag
+                            break  # exit loop early to save time
 
-                else:
-                    commonPrefix = strings[0][:i]
+                    if finished:
+                        break
 
-        return commonPrefix  # return longest common prefix using first element in the list
+                    else:
+                        commonPrefix = strings[0][:i]
+
+            return commonPrefix  # return longest common prefix using first element in the list
 
 
 # TESTS
