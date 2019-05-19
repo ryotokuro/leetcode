ad9b10c3753cb96e5e07b4b47bd579596b697f79
ryotokuro <ryotokuro@gmail.com>
2019-05-19 11:21:00 +1000

    fixed off by one range, fixed boolean list condition

--- Unsolved/longestCommonPrefix.py
+++ Unsolved/longestCommonPrefix.py
@@ -11,16 +11,16 @@ def longestCommonPrefix(strings):
 
         # else, there is more than one string in the array
         else:
-            # to avoid overflow, we use the length of the smallest word in the list
+            # if the list is empty, we want to return an empty string
             commonPrefix = ""
-
+            # print(strings[1][:1], strings[0][:1], list(range(len(min(strings, key=len))+1)))
             # note: in python empty sequences are FALSE
-            if not strings:  # if the string is not empty (implicit boolean when string is empty)
-                shortLen = min(strings, key=len)
+            if strings:  # if the string is not empty (implicit boolean when string is empty)
+                shortLen = min(strings, key=len)  # short the length of the shortest
                 finished = False
 
                 # procedurally need to compare letters
-                for i in range(1, len(shortLen)):
+                for i in range(1, len(shortLen)+1):
                     testPrefix = strings[0][:i]
 
                     for j in range(1, len(strings)):
@@ -40,4 +40,5 @@ def longestCommonPrefix(strings):
 # TESTS
 print(longestCommonPrefix(["flower", "flow", "flight"]))  # Output "fl"
 print(longestCommonPrefix(["dog", "racecar", "car"]))     # Output: ""
-print("heck" + longestCommonPrefix([]))
+print(longestCommonPrefix(["c", "c"]))                    # Output: "c"
+#print("heck" + longestCommonPrefix([]))
