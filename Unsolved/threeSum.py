f851e2ba0eb01e8695ea03a2f9797782140f2422
ryotokuro <ryotokuro@gmail.com>
2019-06-04 23:59:30 +1000

    definitely changing strategy i cant resolve it like this

--- Unsolved/threeSum.py
+++ Unsolved/threeSum.py
@@ -23,6 +23,8 @@ def threeSum(numArray: list[int]):
         map[] = numArray
 
     for i in range(numArray):
+        if numArray[i] +  == 0:
+
         # look for the -x or +y which makes 0 and see if they exist in the list
         # if so then create
 
