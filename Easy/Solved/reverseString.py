c7276be5df008fcf474a9ddde101f35b51d5a2e4
ryotokuro <ryotokuro@gmail.com>
2019-08-18 23:11:28 +1000

    solved reverseString without .reverse()

---
+++ Easy/Solved/reverseString.py
@@ -0,0 +1,15 @@
+def reverseString(s):
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    i = 0
+    j = len(s)-1
+    while i < j:
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1
+    print(s)
+
+
+reverseString(list('pizza'))
+reverseString(list('apples'))
