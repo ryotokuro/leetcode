b9a2d6f3fa85a72bc840a3d42d8bbd216188dbb7
ryotokuro <ryotokuro@gmail.com>
2019-08-21 23:43:13 +1000

    added a new method that doesn't use temp variables

--- Easy/reverseLinkedList.py
+++ Easy/reverseLinkedList.py
@@ -1,4 +1,5 @@
-# link:
+# link: https://leetcode.com/problems/reverse-linked-list/
+
 
 # Definition for singly-linked list.
 class ListNode:
@@ -8,20 +9,26 @@ class ListNode:
 
 
 def reverseList(head: ListNode) -> ListNode:
-    if head is None:
-        return head
+    # METHOD 1: very simple with temp variable
+    # if head is None:
+    #     return head
+    #
+    # # set up
+    # prev = None
+    # curr = head
+    # tmp = head.next
+    #
+    # while curr.next:
+    #     curr.next = prev
+    #     prev, curr, tmp = curr, tmp, tmp.next
+    #
+    # # once exited redirect the head
+    # curr.next = prev
+    # head = curr
+    # return head
 
-    # set up
-    prev = None
-    curr = head
-    tmp = head.next
-
-    while curr.next:
-        curr.next = prev
-        prev, curr, tmp = curr, tmp, tmp.next
-
-    # once exited redirect the head
-    curr.next = prev
-    head = curr
-
-    return head
+    # METHOD 2: no temp variables (Slightly less memory but same speed)
+    new_head = None
+    while head:
+        head.next, head, new_head = new_head, head.next, head
+    return new_head
