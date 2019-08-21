a01cf27bf7255aca12931ea42de6fbf05ee9387f
ryotokuro <ryotokuro@gmail.com>
2019-08-21 23:58:20 +1000

    added new problem, moved reverse linked list to solved

---
+++ Easy/deleteNodeLinkedList.py
@@ -0,0 +1,13 @@
+# link: https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+# PROBLEM
+# Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+#
+# Given linked list -- head = [4,5,1,9], which looks like following:
+# https://assets.leetcode.com/uploads/2018/12/28/237_example.png
+
+# EXAMPLE
+# Input: head = [4,5,1,9], node = 5
+# Output: [4,1,9]
+# Explanation:
+# - You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
