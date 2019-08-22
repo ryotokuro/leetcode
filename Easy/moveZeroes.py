89c873d5ad891cc57658df2ee23392c91a948118
ryotokuro <ryotokuro@gmail.com>
2019-08-22 23:58:39 +1000

    added new problem and details on problem

--- Easy/moveZeroes.py
+++ Easy/moveZeroes.py
@@ -1,2 +1,8 @@
 # link: https://leetcode.com/problems/move-zeroes/
 
+# PROBLEM
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+#
+# EXAMPLE
+# Input: [0,1,0,3,12]
+# Output: [1,3,12,0,0]
