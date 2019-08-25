07569f3a26654bbc8712fad430dd2c1bac25a5d9
ryotokuro <ryotokuro@gmail.com>
2019-08-25 23:25:59 +1000

    added new problem

---
+++ Easy/Solved/validParentheses.py
@@ -0,0 +1,38 @@
+# link: https://leetcode.com/problems/valid-parentheses/
+
+# PROBLEM
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#
+# An input string is valid if:
+# 1. Open brackets must be closed by the same type of brackets.
+# 2. Open brackets must be closed in the correct order.
+#
+# Note: an empty string is also considered valid.
+
+# EXAMPLE
+# Input: "()"
+# Output: true
+
+# EXAMPLE 2
+# Input: "()[]{}"
+# Output: true
+
+# EXAMPLE 3
+# Input: "(]"
+# Output: false
+
+# EXAMPLE 4
+# Input: "([)]"
+# Output: false
+
+# EXAMPLE 5
+# Input: "{[]}"
+# Output: true
+
+
+def validParentheses():
+    return True
+
+
+# TESTS
+validParentheses('()')  # true
