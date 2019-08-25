3b6dbc9c8ae51e53cd4f0ac2278407794139eab2
ryotokuro <ryotokuro@gmail.com>
2019-08-25 18:04:15 +1000

    found the bug, was going through s_chars lol. but great, this is O(n) solution using dictionaries to compare letter freq

--- Easy/validAnagram.py
+++ Easy/validAnagram.py
@@ -19,7 +19,6 @@
 
 
 def validAnagram(s, t):
-    print()
     print(s, t)  # self-testing purposes
 
     # EDGE CASES
@@ -29,21 +28,21 @@ def validAnagram(s, t):
     # If its empty then return True? I guess
     if s == t == '':
         return True
-
     # So from here we know that they're both the same length and non-empty
-    # USING A DICTIONARY TO COMPARE LETTER FREQUENCIES
-    # sorting is a bottleneck so lets remove that
-    # simply store count of letters in each string in a dict
-    # constant lookup so we can just compare dicts
-    s_chars = {}
-    t_chars = {}
-    for i in s:
-        s_chars[i] = s_chars.get(i, 0) + 1
 
-    for j in t:
-        t_chars[j] = s_chars.get(j, 0) + 1
+    # HASH-MAP TO COMPARE LETTER FREQUENCIES (64ms)
+    # Sorting is a bottleneck that runs O(nlogn) so we remove it
+    # Simply store count of letters in each string in a dict
+    s_chars = {}  # create a dictionary for ss
+    t_chars = {}  # and a separate one (needs to be on different lines)
+    for i in s:
+        s_chars[i] = s_chars.get(i, 0) + 1  # if not in dict, set to 1
+
+    for j in t:  # do the same for string t
+        t_chars[j] = t_chars.get(j, 0) + 1
+
     print(s_chars, t_chars, sep='\n')
-    return s_chars == t_chars
+    return s_chars == t_chars  # now we can simply compare and see if they hold the same letters :)
 
     # SLOW (80ms, O(nlogn) solution due to timsort (worst case))
     # s = list(s)
@@ -60,7 +59,9 @@ def validAnagram(s, t):
     # return True
 
 
-# s, t = input().split(' ')
+# TEST CASES
 print(validAnagram('anagram', 'nagaram'))  # True
+print()
 print(validAnagram('rat', 'car'))  # False
-print(validAnagram('a', 'a'))  # False
+print()
+print(validAnagram('a', 'b'))  # False
