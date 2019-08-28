4ee427028c7c4bfd7b40eb85ff3e49c3a5799d41
ryotokuro <ryotokuro@gmail.com>
2019-08-28 20:59:39 +1000

    added solution to mergeTwoSortedLists

--- Easy/Solved/mergeTwoSortedLists.py
+++ Easy/Solved/mergeTwoSortedLists.py
@@ -14,15 +14,35 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+    #def insert(self, data):
 
 
 def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    head = tail = ListNode(0)
 
-    return l2
+    while l1 and l2:
+        if l1.val <= l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+
+    tail.next = l1 if l1 else l2
+    return head.next
 
 
-l1 = ListNode()
-l1.val = 1
-l2 = ListNode()
+l1 = ListNode(1)
+l2 = ListNode(2)
+l1.next = l2
+node = l1.head
+#l1.next.val = 2
+#l1.next.next.val = 4
+
+l2 = ListNode(1)
 l2.val = 1
+#l1.next.val = 3
+#l1.next.next.val = 4
+print(l1, l2.val)
 mergeTwoLists(l1, l2)
