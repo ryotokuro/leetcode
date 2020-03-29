02439fc258c820998efdf85d83874b822e4f2782
ryotokuro <ryotokuro@gmail.com>
2020-03-29 23:52:55 +1100

    going to do bestTime 1 first before tackling bestTime 2

---
+++ Easy/bestTime1.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+def maxProfit(prices):
+    """List[int] -> int
+        Finds max profit from buying one stock and selling it"""
+    max_profit = 0
+
+    return max_profit
+
+# Tests
+maxProfit([7,1,5,3,6,4]) # 5
+
+maxProfit([7,6,4,3,1]) # 0
