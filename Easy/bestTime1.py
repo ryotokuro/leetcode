e1b86d1b7bb88a3efa6f6f88c1f180a33cfdbec4
ryotokuro <ryotokuro@gmail.com>
2020-04-06 22:59:58 +1000

    added o(n^2) solution - terribly inefficient

--- Easy/bestTime1.py
+++ Easy/bestTime1.py
@@ -4,11 +4,18 @@ def maxProfit(prices):
     """List[int] -> int
         Finds max profit from buying one stock and selling it"""
     max_profit = 0
-    for price in prices:
-        
+    profits = []
+    for i in range(len(prices)):
+        for j in range(i, len(prices)):
+            profits.append(prices[i] - prices[j])
+    print(profits)
+    max_profit = abs(min(min(profits), max_profit))
     return max_profit
 
-# Tests
-maxProfit([7,1,5,3,6,4]) # 5
 
-maxProfit([7,6,4,3,1]) # 0
+# Tests
+s = maxProfit([7,1,5,3,6,4]) # 5
+print(s)
+
+s = maxProfit([7,6,4,3,1]) # 0
+print(s)
