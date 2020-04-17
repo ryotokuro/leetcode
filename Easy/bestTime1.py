39ac7eec1453ce2b4aa2a7a7a33e1694e13bca1f
ryotokuro <ryotokuro@gmail.com>
2020-04-18 02:26:39 +1000

    figured out a one-pass solution! :) need to clean up and blog about it

--- Easy/bestTime1.py
+++ Easy/bestTime1.py
@@ -12,6 +12,23 @@ def maxProfit(prices):
     max_profit = max(max(profits), 0)
     return max_profit
 
+def maxProfit(prices):
+    i = 0
+    j = 1
+    max_profit = 0
+    while j in range(len(prices)):
+        max_profit = max(prices[j] - prices[i], max_profit)
+        
+        # i has the potential to keep growing
+        if prices[j] - prices[i] > 0:
+            # keep i
+            pass
+        else:
+            # we want to move i
+            i = j
+        j += 1
+        
+    return max_profit
 
 # Tests
 s = maxProfit([7,1,5,3,6,4]) # 5
