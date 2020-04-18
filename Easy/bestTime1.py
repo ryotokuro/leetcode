b6b71c231da9212b4b87070a0f3fd387d9d7c2d8
ryotokuro <ryotokuro@gmail.com>
2020-04-18 23:51:23 +1000

    cleaned up variable names, added comments

--- Easy/bestTime1.py
+++ Easy/bestTime1.py
@@ -2,33 +2,23 @@
 
 def maxProfit(prices):
     """List[int] -> int
-        Finds max profit from buying one stock and selling it"""
-    profits = []
-    for i in range(len(prices)):
-        for j in range(i, len(prices)):
-            profits.append(prices[j] - prices[i])
-
-    print(profits)
-    max_profit = max(max(profits), 0)
-    return max_profit
-
-def maxProfit(prices):
-    i = 0
-    j = 1
+    Finds max profit from buying one stock and selling it"""
+    buy = 0
+    sell = 1
     max_profit = 0
-    while j in range(len(prices)):
-        max_profit = max(prices[j] - prices[i], max_profit)
+    while sell in range(len(prices)):
+        profit = prices[sell] - prices[buy]
+        max_profit = max(profit, max_profit)
         
-        # i has the potential to keep growing
-        if prices[j] - prices[i] > 0:
-            # keep i
-            pass
-        else:
-            # we want to move i
-            i = j
-        j += 1
+        # the buy stock still has potential to grow
+        if profit < 0:
+            # change to a buy stock with higher potential
+            buy = sell
+        # move sell stock (j) through the list (days)
+        sell += 1
         
     return max_profit
+
 
 # Tests
 s = maxProfit([7,1,5,3,6,4]) # 5
